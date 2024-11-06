98be1cf65d7e8764dd2dfceacf52504c90c2affe
varshitakolipaka <varshita2003@gmail.com>
2024-11-06 17:03:37 +0530

    Logging current status - added analysis code, etc

---
+++ analysis.py
@@ -0,0 +1,4 @@
+from get_metric import *
+
+# generate_report("ood", ["result_dumps/gpt_responses_christian_4o_ood_new_diff_prompt.json", "result_dumps/gpt_responses_muslim_4o_ood_new_diff_prompt.json"])
+generate_report("id", ["result_dumps/gpt_responses_id.json", "result_dumps/gpt_responses_id_c.json"])
